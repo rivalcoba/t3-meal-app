c4fe45dd0abaa6207e88b35a9d5b714cf2aed4fc
rivalcoba <ivan.rivalcoba@gmail.com>
2022-08-19 20:17:21 -0500

    Creando Hero Page

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+// Importando Tipos
+import 'materialize-css'
+// Importando Materilize Css
+import 'materialize-css/dist/css/materialize.min.css'
+import "materialize-css/dist/js/materialize.min.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+ M.AutoInit();
+// Images
+import hero from './assets/images/food-bkg.jpg'
 
-setupCounter(document.querySelector('#counter'))
+// Hero page
+const heroImage = document.getElementById("hero-image");
+console.log(heroImage);
+heroImage.src = hero;
+
