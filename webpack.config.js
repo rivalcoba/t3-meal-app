6134654c5eaf949faa57609359890fcff9ca1ad2
Carla reyes <carlareyesgo@gmail.com>
2022-08-29 13:40:37 -0600

    Cargaddores al archivo de config de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -34,9 +34,14 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-        // More information here https://webpack.js.org/guides/asset-modules/
-        type: "asset",
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
     ]
   }
