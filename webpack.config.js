d263584f30c245f046390225ea3aa34952d9fd0d
Carla reyes <carlareyesgo@gmail.com>
2022-08-30 22:29:42 -0600

    arreglo webpack

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,11 @@ module.exports = {
         ]
       },
       {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        // More information here https://webpack.js.org/guides/asset-modules/
+        type: "asset",
+      },
+      {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
