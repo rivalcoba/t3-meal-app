f5886e2e3f5c359ecacf0a48a9ee32e205196388
Alberto Salas <mlaberto24@icloud.com>
2022-09-01 00:57:24 -0500

    Anadir efecto blur utilizando JS

--- src/js/results.js
+++ src/js/results.js
@@ -1,2 +1,15 @@
 import '../assets/results.css'
 console.log("results JS 🤘");
+
+const randomButton = document.querySelector("#randomButton");
+const blurModal = document.querySelector(".blurModal");
+
+const recipeContainer = document.querySelector(".container");
+
+randomButton.addEventListener("mouseover", () => {
+  blurModal.style.display = "block";
+});
+
+randomButton.addEventListener("mouseout", () => {
+  blurModal.style.display = "none";
+});
