a31be153b6581bbea0da51f10157e0bfcd56bb9f
Carla reyes <carlareyesgo@gmail.com>
2022-09-01 00:15:32 -0600

    Inicio y Configuracion

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import '../assets/main.css'
+import backgroundMain from '../assets/img/background.svg';
+import principalMain from '../assets/img/imagen_principal.svg';
+
+document.getElementById('backgroundMain').style.backgroundImage = `url(${backgroundMain})`;
+/* document.getElementById('principalMain') */
+
+// document.body.style.background = `url(${backgroundMain})`
+
+console.log(principalMain)
