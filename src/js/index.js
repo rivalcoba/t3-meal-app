bb6d0d5ab823a7958344c409ab359e7187bb84de
rivalcoba <ivan.rivalcoba@gmail.com>
2022-08-31 20:03:33 -0500

    Limpiando index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,9 @@
 import '../assets/index.css'
 import heroImage from '../assets/img/food_test.jpg';
-// import ingredients from './ingredients.js';
 
 // Testing image
 let heroImageTag = document.getElementById('hero');
 heroImageTag.src = heroImage;
 
 // Testing Script
-console.log("Script Funcionando");
-
-// Script selector
-// window.location.pathname === '/ingredients.html'
-  // && (function () { console.log("HI FROM") })()
+console.log("Script index.js Funcionando");
