5a9f87964aceaadae635d32c528387b81deb1b06
Alberto Salas <mlaberto24@icloud.com>
2022-09-02 14:21:43 -0500

    Anadir animacion para receta aleatoria

--- src/js/results.js
+++ src/js/results.js
@@ -1,5 +1,15 @@
 import '../assets/results.css'
+import pandaImage from "../assets/img/panda.svg";
+import rectangleImage from "../assets/img/rectangle9.svg";
 console.log("results JS 🤘");
+
+// Panda Image
+const panda = document.querySelector("#pandaImage");
+panda.src = pandaImage;
+
+// Rectangle
+const rectangle = document.querySelector("#rectangle");
+rectangle.src = rectangleImage;
 
 const randomButton = document.querySelector("#randomButton");
 const blurModal = document.querySelector(".blurModal");
