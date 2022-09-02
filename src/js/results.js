ee291a808e67df94320f3b4647d2ca9c611cee61
Alberto Salas <mlaberto24@icloud.com>
2022-09-02 16:12:07 -0500

    Corregir alineamiento de recenta aleatoria

--- src/js/results.js
+++ src/js/results.js
@@ -11,15 +11,44 @@ panda.src = pandaImage;
 const rectangle = document.querySelector("#rectangle");
 rectangle.src = rectangleImage;
 
+// Random Button
 const randomButton = document.querySelector("#randomButton");
+
+// Blur
 const blurModal = document.querySelector(".blurModal");
 
+// Container
 const recipeContainer = document.querySelector(".container");
 
+// randomRecipeName
+const randomRecipeName = document.querySelector(".randomRecipeName");
+
+// Event listeners for randomButton
 randomButton.addEventListener("mouseover", () => {
   blurModal.style.display = "block";
+
+  // Gets new recipe every time the mouse is over randomButton
+  getRandomRecipe().then(function (data) {
+    console.log(data);
+    const name = getRecipeName(data);
+
+    randomRecipeName.textContent = name;
+  });
 });
 
 randomButton.addEventListener("mouseout", () => {
   blurModal.style.display = "none";
 });
+
+// API Helper Functions
+function getRandomRecipe() {
+  return fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+  .then(function (response) {
+    return response.json();
+  });
+}
+
+function getRecipeName(data) {
+  return data.meals[0].strMeal;
+}
+
