bdcfa0a3f479571b289499642119b8c8853ac93e
barsaiz19 <161447@mail.unacar.mx>
2022-09-04 14:23:22 -0500

    agrega los cambios que se realizaron por ultima vez en la rama 16

--- src/js/index.js
+++ src/js/index.js
@@ -11,11 +11,60 @@ document.getElementById("backgroundMain").src = backgroundMain;
 document.getElementById("principalMain").src = principalMain;
 document.getElementById("aleatorioIcon").src = aleatorioIcon;
 
-document.getElementById("searchForm").addEventListener("submit", (event) => {
-    event.preventDefault();
-    alert("Search");
-})
+// Leer la entrada del usuario
 
-document.getElementById("randomButton").addEventListener("click",(event) => {
-    window.location.href = "/ingredients.html";
-})
+const startSearch = document.querySelector("#startSearchBtn");
+const stringSearch = document.querySelector("#inputMeal");
+const toInsertChild = document.querySelector("body");
+let sugContainer = document.querySelector(".sugContainer");
+
+let terminoBusqueda;
+
+let sugerenciasArray = [];
+
+stringSearch.addEventListener("input", function leerBusqueda(event) {
+  terminoBusqueda = event.target.value;
+  imprimirSugerencia(terminoBusqueda);
+});
+
+function limpiarArray() {
+  // Eliminar datos del array
+  console.log("Array Limpiado");
+  return (sugerenciasArray = []);
+}
+
+function llenarArray(nameMeal, idMeal) {
+  //Insertar nuevos datos en el array
+  sugerenciasArray.push({
+    nameMeal: nameMeal,
+    idMeal: idMeal,
+  });
+  console.log(sugerenciasArray);
+}
+
+function traerSugerencias(terminoBusqueda) {
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${terminoBusqueda}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      limpiarArray();
+      for (let i = 0; i < 4; i++) {
+        //   console.log(data.meals[i].idMeal);
+        //   console.log(data.meals[i].strMeal);
+        llenarArray(data.meals[i].strMeal, data.meals[i].idMeal);
+      }
+    })
+    .catch(function (error) {
+      console.log("Error en la API");
+    });
+}
+
+function imprimirSugerencia(terminoBusqueda) {
+  if (terminoBusqueda.length < 2) {
+    sugContainer.style.display = "none";
+  } else {
+    sugContainer.style.display = "flex";
+    traerSugerencias(terminoBusqueda);
+  }
+}
